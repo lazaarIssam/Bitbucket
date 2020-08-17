302a185b3268b6ffb04abfe957365cc18d4e1f42
Lazaar Issam <342-li195592@users.noreply.www-apps.univ-lehavre.fr>
2020-08-17 15:59:20 +0200

    change after_login page

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -22,6 +22,7 @@ class RouteServiceProvider extends ServiceProvider
      * @var string
      */
     public const HOME = '/home';
+    public const COMPANY = '/companies';
 
     /**
      * Define your route model bindings, pattern filters, etc.
