7a3761ef1211d8eed9f510b69af796f63ea6e3a5
Lazaar Issam <342-li195592@users.noreply.www-apps.univ-lehavre.fr>
2020-08-17 19:44:13 +0200

    final touches

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -30,13 +30,7 @@
     <div id="app">
         <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
             <div class="container">
-                <span class="badge badge-pill badge-info"> &nbsp;&nbsp;&nbsp;&nbsp;<a class="navbar-brand" href="{{ url('/') }}">Home</a></span> &nbsp;&nbsp;
-                <a class="navbar-brand" href="{{ url('/companies') }}">
-                    Gestion des companies
-                </a>
-                <a class="navbar-brand" href="{{ url('/employees') }}">
-                    Gestion des employees
-                </a>
+                <span class="badge badge-pill badge-info"> &nbsp;&nbsp;&nbsp;&nbsp;<a class="navbar-brand" href="{{ url('/') }}">Home</a></span> 
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
                 </button>
@@ -44,9 +38,7 @@
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <!-- Left Side Of Navbar -->
                     <ul class="navbar-nav mr-auto">
-
                     </ul>
-
                     <!-- Right Side Of Navbar -->
                     <ul class="navbar-nav ml-auto">
                         <!-- Authentication Links -->
@@ -61,18 +53,24 @@
                                 </li>
                             @endif --}}
                         @else
+                        <ul class="navbar-nav mr-auto">
+                            <a class="navbar-brand" href="{{ url('/companies') }}">
+                                Gestion des companies
+                            </a>
+                            <a class="navbar-brand" href="{{ url('/employees') }}">
+                                Gestion des employees
+                            </a>
+                        </ul>
                             <li class="nav-item dropdown">
                                 <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
                                     {{ Auth::user()->name }} <span class="caret"></span>
                                 </a>
-
                                 <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
                                     <a class="dropdown-item" href="{{ route('logout') }}"
                                        onclick="event.preventDefault();
                                                      document.getElementById('logout-form').submit();">
                                         {{ __('Logout') }}
                                     </a>
-
                                     <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
                                         @csrf
                                     </form>
