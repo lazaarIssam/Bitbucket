10fd519c169a3108fc74fa71f7c17ddd8cfe1119
Lazaar Issam <342-li195592@users.noreply.www-apps.univ-lehavre.fr>
2020-08-17 14:02:06 +0200

    Migration files

---
+++ database/migrations/2020_08_17_114009_create_employees_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateEmployeesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('employees', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('FirstName',120);
+            $table->string('LastName',120);
+            $table->string('Designation');
+            $table->string('Email');
+            $table->text('Address');
+            $table->tinyInteger('Mobile');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('employees');
+    }
+}
